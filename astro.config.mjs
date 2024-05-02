16bd873cd08761c26d2847520be0638d17c6e662
layout-ch <webmaster@layout.ch>
2024-05-02 16:19:44 +0200

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,23 @@
 import { defineConfig } from 'astro/config';
 import liciousI18n from "@astrolicious/i18n";
-
 import { sanityIntegration } from "@sanity/astro";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
+  adapter: vercel(),
   integrations: [liciousI18n({
     defaultLocale: "fr",
     locales: ["fr", "de", "en"],
     strategy: 'prefix',
     sitemap: true
-  }), sanityIntegration(
-    {
-      projectId: "54p00593",
-      dataset: "production",
-      useCdn: false,
-      studioBasePath: "/admin"
-    }
-  ), react()]
+  }), sanityIntegration({
+    projectId: "54p00593",
+    dataset: "production",
+    useCdn: false,
+    studioBasePath: "/admin"
+  }), react()]
 });
